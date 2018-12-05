9e36c07618c0a11fd623cd65c76b9c74cb81b9ce
zhouxindong <zhouxindong@163.com>
2018-12-05 11:07:55 +0800

    LZW

---
+++ EasyZip/Lzw/Lzw.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using EasyZip.Core;
+
+namespace EasyZip.Lzw
+{
+    public class Lzw
+    {
+        public static void DecompressFile(string file_fullname)
+        {
+            if (!Path.HasExtension(file_fullname) || Path.GetExtension(file_fullname) != ".Z" ||
+                Path.GetExtension(file_fullname) != ".z")
+                throw new InvalidOperationException("file extension can't recognised");
+            if (!File.Exists(file_fullname))
+                return;
+
+            using (var in_stream = new LzwInputStream(File.OpenRead(file_fullname)))
+            using (
+                var out_file =
+                    File.Create(Path.Combine(Path.GetDirectoryName(file_fullname),
+                        Path.GetFileNameWithoutExtension(file_fullname))))
+            {
+                var buffer = new byte[4096];
+                StreamUtils.Copy(in_stream, out_file, buffer);
+            }
+        }
+    }
+}
---
+++ EasyZipTests/Lzw/LzwInputStreamTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EasyZip.Lzw;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyZip.Lzw.Tests
+{
+    [TestClass()]
+    public class LzwInputStreamTests
+    {
+        [TestMethod]
+        public void ZeroLengthInputStream()
+        {
+            var lis = new LzwInputStream(new MemoryStream());
+            bool exception = false;
+            try
+            {
+                lis.ReadByte();
+            }
+            catch
+            {
+                exception = true;
+            }
+
+            Assert.IsTrue(exception, "reading from an empty stream should cause an exception");
+        }
+    }
+}
